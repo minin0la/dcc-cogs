1d71f945438cef023def895a3080bc81f721954e
Natawat Hebnak <minin0la17@gmail.com>
2023-08-23 23:29:44 +0800

    Updated new Announcer and Blacklist format

--- DCC_Announcer/DCC_Announcer.py
+++ DCC_Announcer/DCC_Announcer.py
@@ -13,6 +13,7 @@ import difflib
 import random
 import string
 
+
 class DCC_ANNOUNCER(commands.Cog):
     """Its all about Downtown Cab Co - Announcer System"""
 
@@ -21,50 +22,114 @@ class DCC_ANNOUNCER(commands.Cog):
         self.database = Config.get_conf(self, identifier=1)
         data = {"Announcer": []}
         self.database.register_guild(**data)
-        self.blacklists = Config.get_conf(None, identifier=1, cog_name='DCC_BLACKLIST')
-    
+        self.blacklists = Config.get_conf(None, identifier=1, cog_name="DCC_BLACKLIST")
+
     @commands.command(pass_context=True, invoke_without_command=True)
     @commands.guild_only()
-    @commands.has_any_role("CEO", "COO", "Bot-Developer", 'General Manager', 'Manager', 'Assistant Manager', 'CFO') 
+    @commands.has_any_role(
+        "CEO",
+        "COO",
+        "Bot-Developer",
+        "General Manager",
+        "Manager",
+        "Assistant Manager",
+        "CFO",
+    )
     async def refresh(self, ctx):
         """Used to refreshed the Announcer message"""
 
         Blacklists = await self.blacklists.guild(ctx.guild).Blacklists()
         Announcers = await self.database.guild(ctx.guild).Announcer()
-        message_board_channel = self.bot.get_channel(669167190334767105) #Get Message Board Channel
+        message_board_channel = self.bot.get_channel(
+            669167190334767105
+        )  # Get Message Board Channel
         blacklist_message = ""
 
-        Blacklists = sorted(Blacklists, key=lambda k: k['NAME'])
+        Blacklists = sorted(Blacklists, key=lambda k: k["NAME"])
 
-        try:
-            for x in Blacklists:
-                blacklist_message = blacklist_message + "- {} ({})\n{}\n".format(x['NAME'], x['DATE'], x['REASON'])
-        except:
-            pass
-        blacklist_message = utils.chat_formatting.pagify(blacklist_message, delims='-', page_length=1000)
-        embed = discord.Embed(colour=discord.Colour(0xff9700), timestamp=datetime.datetime.utcfromtimestamp(int(time.time())))
-        embed.set_thumbnail(url="https://res.cloudinary.com/teepublic/image/private/s--4WWDcpP4--/t_Preview/b_rgb:ffb81c,c_limit,f_jpg,h_630,q_90,w_630/v1468933638/production/designs/84620_4.jpg")
-        embed.set_author(name="Downtown Cab Co. Dispatcher", icon_url="https://res.cloudinary.com/teepublic/image/private/s--4WWDcpP4--/t_Preview/b_rgb:ffb81c,c_limit,f_jpg,h_630,q_90,w_630/v1468933638/production/designs/84620_4.jpg")
+        # blacklist_message = utils.chat_formatting.pagify(
+        #     blacklist_message, delims="-", page_length=1000
+        # )
+        embed = discord.Embed(
+            colour=discord.Colour(0xFF9700),
+            timestamp=datetime.datetime.utcfromtimestamp(int(time.time())),
+        )
+        # embed.set_thumbnail(
+        #     url="https://res.cloudinary.com/teepublic/image/private/s--4WWDcpP4--/t_Preview/b_rgb:ffb81c,c_limit,f_jpg,h_630,q_90,w_630/v1468933638/production/designs/84620_4.jpg"
+        # )
+        embed.set_author(
+            name="Downtown Cab Co. Dispatcher",
+            icon_url="https://res.cloudinary.com/teepublic/image/private/s--4WWDcpP4--/t_Preview/b_rgb:ffb81c,c_limit,f_jpg,h_630,q_90,w_630/v1468933638/production/designs/84620_4.jpg",
+        )
         embed.set_footer(text="Posted by Evelyn")
 
-        embed.add_field(name="**Radio Frequency**", value="```{}```".format(Announcers['MAIN_FREQ']), inline=True)
-        embed.add_field(name="**Emergency Frequency**", value="```{}```".format(Announcers["EMERGENCY_FREQ"]), inline=True)
-        embed.add_field(name="**Passenger Blacklist as of {}**".format(datetime.date.today().strftime("%d/%b/%Y")), value="", inline=False)
-        for x in list(string.ascii_uppercase):
-            blacklist_message_alpha = ""
-            NameExists = False
-            for y in Blacklists:
-                if y['NAME'].upper().startswith(x):
-                    blacklist_message_alpha = blacklist_message_alpha + "- {} ({})\n{}\n".format(y['NAME'], y['DATE'], y['REASON'])
-                    NameExists = True
-            if NameExists == True:
-                    embed.add_field(name="**{}**".format(x), value="```diff\n" + blacklist_message_alpha + "```", inline=False)
-                    
-        # for num, x in enumerate(blacklist_message, 1):
-        #     x = "```diff\n" + x + "```"
-        #     embed.add_field(name="**Passenger Blacklist as of {} (Page {})**".format(datetime.date.today().strftime("%d/%b/%Y"), num), value=x, inline=False)
+        embed.add_field(
+            name="**Radio Frequency**",
+            value="```{}```".format(Announcers["MAIN_FREQ"]),
+            inline=True,
+        )
+        embed.add_field(
+            name="**Emergency Frequency**",
+            value="```{}```".format(Announcers["EMERGENCY_FREQ"]),
+            inline=True,
+        )
+        embed.add_field(
+            name="**Passenger Blacklist as of {}**".format(
+                datetime.date.today().strftime("%d/%b/%Y")
+            ),
+            value="",
+            inline=False,
+        )
         try:
-            the_announcer = await message_board_channel.fetch_message(Announcers['MSG_ID'])
+            OldEmbed = 0
+            EmbedCounter = 0
+            messageList = []
+            for x in list(string.ascii_uppercase):
+                blacklist_message_alpha = ""
+                NameExists = False
+                for y in Blacklists:
+                    if y["NAME"].upper().startswith(x):
+                        blacklist_message_alpha = (
+                            blacklist_message_alpha
+                            + "-{}-\n+({})+\n{}\n".format(
+                                y["NAME"], y["DATE"], y["REASON"]
+                            )
+                        )
+                        NameExists = True
+                if NameExists == True:
+                    blacklist_message_alpha = (
+                        "```diff\n" + blacklist_message_alpha + "```"
+                    )
+                    messageList.append({"LETTER": x, "TEXT": blacklist_message_alpha})
+            for x in messageList:
+                if len(x["TEXT"]) + OldEmbed < 500 and EmbedCounter < 3:
+                    embed.add_field(
+                        name=x["LETTER"],
+                        value=x["TEXT"],
+                        inline=True,
+                    )
+                    OldEmbed = OldEmbed + len(x["TEXT"])
+                    EmbedCounter = EmbedCounter + 1
+                elif EmbedCounter == 3:
+                    embed.add_field(
+                        name=x["LETTER"],
+                        value=x["TEXT"],
+                        inline=True,
+                    )
+                    OldEmbed = 0
+                    EmbedCounter = 0
+                    OldEmbed = OldEmbed + len(x["TEXT"])
+                    EmbedCounter = EmbedCounter + 1
+                else:
+                    embed.add_field(name=x["LETTER"], value=x["TEXT"], inline=False)
+                    OldEmbed = 0
+                    EmbedCounter = 0
+        except:
+            pass
+        try:
+            the_announcer = await message_board_channel.fetch_message(
+                Announcers["MSG_ID"]
+            )
             await the_announcer.delete()
         except:
             pass
@@ -75,37 +140,81 @@ class DCC_ANNOUNCER(commands.Cog):
 
     @commands.command(pass_context=True, invoke_without_command=True)
     @commands.guild_only()
-    @commands.has_any_role("CEO", "COO", "Bot-Developer", 'General Manager', 'Manager', 'Assistant Manager', 'CFO')
+    @commands.has_any_role(
+        "CEO",
+        "COO",
+        "Bot-Developer",
+        "General Manager",
+        "Manager",
+        "Assistant Manager",
+        "CFO",
+    )
     async def radiofreq(self, ctx, main: str, emergency: str):
         Blacklists = await self.blacklists.guild(ctx.guild).Blacklists()
         Announcers = await self.database.guild(ctx.guild).Announcer()
-        message_board_channel = self.bot.get_channel(669167190334767105) #Get Message Board Channel
+        message_board_channel = self.bot.get_channel(
+            669167190334767105
+        )  # Get Message Board Channel
         blacklist_message = ""
 
-        Blacklists = sorted(Blacklists, key=lambda k: k['NAME'])
+        Blacklists = sorted(Blacklists, key=lambda k: k["NAME"])
 
         try:
             for x in Blacklists:
-                blacklist_message = blacklist_message + "- {} ({})\n{}\n".format(x['NAME'], x['DATE'], x['REASON'])
+                blacklist_message = blacklist_message + "- {} ({})\n{}\n".format(
+                    x["NAME"], x["DATE"], x["REASON"]
+                )
         except:
             pass
-        blacklist_message = utils.chat_formatting.pagify(blacklist_message, delims='-', page_length=1000)
-        embed = discord.Embed(colour=discord.Colour(0xff9700), timestamp=datetime.datetime.utcfromtimestamp(int(time.time())))
-        embed.set_thumbnail(url="https://res.cloudinary.com/teepublic/image/private/s--4WWDcpP4--/t_Preview/b_rgb:ffb81c,c_limit,f_jpg,h_630,q_90,w_630/v1468933638/production/designs/84620_4.jpg")
-        embed.set_author(name="Downtown Cab Co. Dispatcher", icon_url="https://res.cloudinary.com/teepublic/image/private/s--4WWDcpP4--/t_Preview/b_rgb:ffb81c,c_limit,f_jpg,h_630,q_90,w_630/v1468933638/production/designs/84620_4.jpg")
+        blacklist_message = utils.chat_formatting.pagify(
+            blacklist_message, delims="-", page_length=1000
+        )
+        embed = discord.Embed(
+            colour=discord.Colour(0xFF9700),
+            timestamp=datetime.datetime.utcfromtimestamp(int(time.time())),
+        )
+        embed.set_thumbnail(
+            url="https://res.cloudinary.com/teepublic/image/private/s--4WWDcpP4--/t_Preview/b_rgb:ffb81c,c_limit,f_jpg,h_630,q_90,w_630/v1468933638/production/designs/84620_4.jpg"
+        )
+        embed.set_author(
+            name="Downtown Cab Co. Dispatcher",
+            icon_url="https://res.cloudinary.com/teepublic/image/private/s--4WWDcpP4--/t_Preview/b_rgb:ffb81c,c_limit,f_jpg,h_630,q_90,w_630/v1468933638/production/designs/84620_4.jpg",
+        )
         embed.set_footer(text="Posted by Evelyn")
 
-        embed.add_field(name="**Radio Frequency**", value="```{}```".format(main), inline=True)
-        embed.add_field(name="**Emergency Frequency**", value="```{}```".format(emergency), inline=True)
+        embed.add_field(
+            name="**Radio Frequency**", value="```{}```".format(main), inline=True
+        )
+        embed.add_field(
+            name="**Emergency Frequency**",
+            value="```{}```".format(emergency),
+            inline=True,
+        )
         for num, x in enumerate(blacklist_message, 1):
             x = "```diff\n" + x + "```"
-            embed.add_field(name="**Passenger Blacklist as of {} (Page {})**".format(datetime.date.today().strftime("%d/%b/%Y"), num), value=x, inline=False)
+            embed.add_field(
+                name="**Passenger Blacklist as of {} (Page {})**".format(
+                    datetime.date.today().strftime("%d/%b/%Y"), num
+                ),
+                value=x,
+                inline=False,
+            )
         try:
-            the_announcer = await message_board_channel.fetch_message(Announcers['MSG_ID'])
+            the_announcer = await message_board_channel.fetch_message(
+                Announcers["MSG_ID"]
+            )
             await the_announcer.edit(embed=embed)
-            await self.database.guild(ctx.guild).Announcer.set({"MSG_ID": Announcers['MSG_ID'], "MAIN_FREQ": main, "EMERGENCY_FREQ": emergency})
+            await self.database.guild(ctx.guild).Announcer.set(
+                {
+                    "MSG_ID": Announcers["MSG_ID"],
+                    "MAIN_FREQ": main,
+                    "EMERGENCY_FREQ": emergency,
+                }
+            )
             await ctx.send("Radio Frequency has been updated")
         except:
             msg = await message_board_channel.send(embed=embed)
-            await self.database.guild(ctx.guild).Announcer.set({"MSG_ID": msg.id, "MAIN_FREQ": main, "EMERGENCY_FREQ": emergency})
+            await self.database.guild(ctx.guild).Announcer.set(
+                {"MSG_ID": msg.id, "MAIN_FREQ": main, "EMERGENCY_FREQ": emergency}
+            )
             await ctx.send("Radio Frequency has been updated")
